c7f751e9dbfc4e58e01480d3325c04daa5a0a1cc
Kehad <keahnney01@gmail.com>
2024-08-15 01:13:25 +0100

    101st csomlmist

--- App.js
+++ App.js
@@ -57,7 +57,7 @@ export default function App() {
                 />
               );
             }}
-            keyExtractor={(item, index) => {
+            keyExtractor={(item) => {
               return item.id;
             }}
             alwaysBounceVertical={false}
